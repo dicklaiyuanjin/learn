72a42b012688af85cae7da17a3aaa3fb15e2f13c
Dick <549519649@qq.com>
2019-08-08 19:22:55 +0800

    rpc1: simple example

---
+++ rpc1/server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "net"
+  "net/rpc"
+  "log"
+  "learn/rpc1/hello"
+)
+
+func main() {
+  rpc.RegisterName("HelloService", new(hello.HelloService))
+
+  listener, err := net.Listen("tcp", ":1234")
+  if err != nil {
+    log.Fatal("ListenTCP error: ", err)
+  }
+
+  conn, err := listener.Accept()
+  if err != nil {
+    log.Fatal("Accept error: ", err)
+  }
+
+  rpc.ServeConn(conn)
+}
