131def20c410ec83b26b7c7305987939e3818c12
Dick <549519649@qq.com>
2019-08-08 21:27:21 +0800

    simple protobuf

---
+++ protobuf1/client.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "net/rpc"
+  "log"
+  "fmt"
+
+  "learn/protobuf1/hello"
+)
+
+func main() {
+  client, err := rpc.Dial("tcp", "localhost:1234")
+  if err != nil {
+    log.Fatal("dialing: ", err)
+  }
+
+  var reply hello.String
+  err = client.Call("HelloService.Hello", &hello.String{Value: "hello"}, &reply)
+  if err != nil {
+    log.Fatal("calling: ", err)
+  }
+
+  fmt.Println(reply)
+}
---
+++ protobuf1/hello/hello.go
@@ -0,0 +1,8 @@
+package hello
+
+type HelloService struct {}
+
+func (p *HelloService) Hello(request *String, reply *String) error {
+  reply.Value = "hello: " + request.GetValue()
+  return nil
+}
